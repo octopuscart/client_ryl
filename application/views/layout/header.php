45a4e66cdf88e9ea9119d78e68b72936a482a630
octopuscart <octopuscartltd@gmail.com>
2022-09-15 01:42:57 +0530

    add files

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -54,19 +54,7 @@
             <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
             <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
             <![endif]-->
-    </head>
-    <body ng-app="App">
-
-        <!--LOADER--> 
-        <div id="loader">
-            <div class="position-center-center">
-                <img src="<?php echo base_url(); ?>assets/theme/images/crown.gif"  style="    height: 50px;">
-                <!--<div class="loader"></div>-->
-            </div>
-        </div>
-
-
-
+        
         <script>
 
 
@@ -103,10 +91,23 @@
             }
 
         </style>
+    </head>
+    <body ng-app="App"  ng-controller="ShopController">
+
+        <!--LOADER--> 
+        <div id="loader">
+            <div class="position-center-center">
+                <img src="<?php echo base_url(); ?>assets/theme/images/crown.gif"  style="    height: 50px;">
+                <!--<div class="loader"></div>-->
+            </div>
+        </div>
+
+
+
 
 
         <!-- Page Wrapper -->
-        <div id="wrap" class="shop-page" ng-controller="ShopController"> 
+        <div id="wrap" class="shop-page"> 
             <!-- Rights -->
             <div class="rights  linkheader hideonmobile " style="    padding: 0;    background: #161409;">
                 <div class="container ">
